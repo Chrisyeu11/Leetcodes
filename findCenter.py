223d974a071ce50fad177cf86f4748f3038bd708
Chris <122718192+Chrisyeu11@users.noreply.github.com>
2024-06-26 22:16:35 -0500

    Create findCenter.py

---
+++ findCenter.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        # Check the first two edges
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
